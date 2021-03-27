d9680047b1e3855eac44a6a7f8c9363a053cfd38
Jack Getman <Jack.Getman@h2obridge.com>
2021-03-27 14:50:33 -0500

    vary marker size

--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -14,5 +14,13 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tile
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map); 
 
-//  Add a marker to the map for Los Angeles, California.
-let marker = L.marker(L.circleMarker([34.0522, -118.2437]).addTo(map));
+// An array containing each city's location, state, and population.
+let cities =city_data;
+
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location, {radius:city.population/100000})
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
+});
