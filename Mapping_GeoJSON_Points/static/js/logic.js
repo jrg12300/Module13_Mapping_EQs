9058bc2c759827f2d1a936223d925f0337f3c148
Jack Getman <Jack.Getman@h2obridge.com>
2021-03-27 22:00:23 -0500

    toronto neighborhoods

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -4,7 +4,7 @@ console.log("working");
 //let map = L.map('mapid').setView([30, 30], 2);
 
  // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
@@ -34,6 +34,8 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     streets.addTo(map); 
 
 
+    // Accessing the Toronto airline routes GeoJSON URL.
+    let torontoData = "https://raw.githubusercontent.com/jrg12300/Module13_Mapping_EQs/Mapping_GeoJSON_Points/torontoRoutes.json";
 
 
 
@@ -41,12 +43,23 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
     let airportData = 'https://raw.githubusercontent.com/jrg12300/Module13_Mapping_EQs/Mapping_GeoJSON_Points/majorAirports.json' ;
     console.log(airportData);
 
+    // Create a style for the lines.
+    let myStyle = {color: "#ffffa1",weight: 2};
 
     // Grabbing our GeoJSON data.
-    d3.json(airportData).then(function(data) {
+    d3.json(torontoData).then(function(data) {
         console.log(data);
     // Creating a GeoJSON layer with the retrieved data.
-    L.geoJson(data).addTo(map);
+    L.geoJson(data,
+        {
+            style: myStyle,
+            OnEachFeature: function(feature,layer)
+            {
+                layer.bindPopup("<h3> Airline: " + feature.properties.airline + '</h3> <hr> <h3> Destination: ' + feature.properties.dst + '</h3>');
+            }
+        }
+        )
+        .addTo(map);
     });
 
 
